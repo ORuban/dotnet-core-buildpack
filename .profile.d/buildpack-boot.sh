64bf47279fc0802e8db5f114a1535cc44ec784e7
oruban <settlers.net@gmail.com>
2017-01-31 22:34:23 +0200

    remove redundant logic

--- .profile.d/buildpack-boot.sh
+++ .profile.d/buildpack-boot.sh
@@ -1,5 +1,4 @@
 export PATH="$PATH:$HOME/dotnet:$HOME/.apt/usr/bin"
-export PATH="$PATH:$HOME/node_modules"
 export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$HOME/.apt/usr/lib/x86_64-linux-gnu:$HOME/.apt/usr/lib/i386-linux-gnu:$HOME/.apt/usr/lib"
 export LIBRARY_PATH="$LIBRARY_PATH:$HOME/.apt/usr/lib/x86_64-linux-gnu:$HOME/.apt/usr/lib/i386-linux-gnu:$HOME/.apt/usr/lib"
 export INCLUDE_PATH="$INCLUDE_PATH:$HOME/.apt/usr/include"
